31cf0f33be090740ac3b9d35607913fe7346b2ef
Nicola Ruaro <ruaronicola@gmail.com>
2017-10-08 10:44:30 +0200

    fix label rendering

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
         })
     ).then(function( data, textStatus, jqXHR ){
         // Inject doughnut labels data
-        $("#chart-1-labels").text(stats["valid_https_perc"])
-        $("#chart-2-labels").text(stats["defaults_to_https_perc"])
+        $("#chart-1-label").text(stats["valid_https_perc"])
+        $("#chart-2-label").text(stats["defaults_to_https_perc"])
 
         // Initialize doughnut charts
         options = { 
